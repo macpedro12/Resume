435c93fc0ff7d34a5b877820c2c0dc41e760ae0b
macpedro12 <pedroamorim62@gmail.com>
2021-08-05 13:12:29 -0300

    Finished Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,9 @@ import styles from "../styles/index.module.scss";
 export default function Home() {
   return (
     <div className={styles.all}>
-      <Image
-        src="/Background.png"
-        layout="fill"
-        className={styles.background}
-        objectFit="fill"
-      />
-
+      <div className={styles.bg}>
+        <img src="/Background.png" className={styles.stretch}/>
+      </div>
       <div className={styles.title}>
         <p>
           <span className={styles.differentColor}>&lt;h1&gt;</span>
@@ -20,10 +16,10 @@ export default function Home() {
         </p>
         <h2>Bem vindo ao Meu Currículo</h2>
       </div>
-      <div className={styles.link}>
+      <div div className={styles.link}>
         <div className={styles.link2}>
           <img src="/yellowPoint.svg" className={styles.yellowPoint} />
-          <Link href="/">
+          <Link href="/habilities">
             <a>Habilidades</a>
           </Link>
         </div>
@@ -34,7 +30,7 @@ export default function Home() {
           </Link>
         </div>
         <div className={styles.link2}>
-          <img src="/greenPoint.svg" className={styles.greenPoint}  />
+          <img src="/greenPoint.svg" className={styles.greenPoint} />
           <Link href="/">
             <a>Contato</a>
           </Link>
